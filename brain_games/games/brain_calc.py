8e157e56572863224833c43cb1a1de73c6effe09
Laura609 <laura.metnietse@gmail.com>
2024-12-24 16:57:35 +0300

    Fix brain-calc

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,6 +1,5 @@
 import random
 import operator
-import sys
 
 OPERATIONS = {
     '+': operator.add,
@@ -8,28 +7,28 @@ OPERATIONS = {
     '*': operator.mul,
 }
 
+
 def generate_question():
-    """Генерация случайного математического выражения и правильного ответа"""
+    """Генерирует случайный вопрос и правильный ответ для игры 'Калькулятор'."""
     num1 = random.randint(1, 100)
     num2 = random.randint(1, 100)
     operation = random.choice(list(OPERATIONS.keys()))
     question = f"{num1} {operation} {num2}"
-    correct_answer = OPERATIONS[operation](num1, num2)
-    return question, str(correct_answer)
+    correct_answer = str(OPERATIONS[operation](num1, num2))
+    return question, correct_answer
 
-def play_game():
-    """Основная логика игры"""
+
+def brain_calc():
+    """Запуск игры 'Калькулятор' с 3 раундами."""
+    print("Welcome to the Brain Games!")  # Приветствие перед началом игры
+    name = input("May I have your name? ")  # Получаем имя игрока
+    print(f"Hello, {name}!")
+
     rounds = 3
-    print("Welcome to the Brain Games!")  # Приветствие
-    name = input("May I have your name? ")  # Запрос имени
-    print(f"Hello, {name}!")  # Приветствие игрока
-    print("What is the result of the expression?")
-
-    # Начало игрового процесса: цикл вопросов
     for _ in range(rounds):
-        question, correct_answer = generate_question()  # Генерация вопроса
-        print(f"Question: {question}")  # Вопрос в нужном формате
-        user_answer = input("Your answer: ")  # Ответ игрока
+        question, correct_answer = generate_question()
+        print(f"Question: {question}")  # Печатаем вопрос в нужном формате
+        user_answer = input("Your answer: ")
 
         if user_answer == correct_answer:
             print("Correct!")
@@ -37,5 +36,4 @@ def play_game():
             print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {name}!")
             return
-
     print(f"Congratulations, {name}!")
